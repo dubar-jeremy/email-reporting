ae909f5a1f6cc372296ce1c01e8ac6d6ad461bc4
Jérémy <dubar.jeremy@gmail.com>
2022-04-12 23:34:13 +0200

    chore(back): remove folder error

--- server/src/employee/error/employee.error.ts
+++
@@ -1,9 +0,0 @@
-import { NotFoundException } from "@nestjs/common"
-
-export class employeeExceptions {
-  constructor() {}
-
-  NOT_FOUND(){
-    throw new NotFoundException('employee not found')
-  }
-}
